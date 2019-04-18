8070713f3aa372fe440cdfc5d2419bfd4b3530e3
xiaywang <42940555+xiaywang@users.noreply.github.com>
2019-04-18 17:04:53 +0200

    Add files via upload

---
+++ dataset/from_physionet/README.md
@@ -0,0 +1,3 @@
+100.txt
+sampling rate 360Hz
+there are in total 3600 sample
---
+++ hamilton/inc/ecgcodes.h
@@ -0,0 +1,17 @@
+/*
+ * ecgcodes.h
+ *
+ *  Created on: Apr 5, 2019
+ *      Author: xiaywang
+ */
+
+#ifndef INC_ECGCODES_H_
+#define INC_ECGCODES_H_
+
+#define NORMAL	1	/* normal beat */
+#define	UNKNOWN	13	/* unclassifiable beat */
+#define	PVC	5	/* premature ventricular contraction */
+
+#define WFDB_MAXSIG 32
+
+#endif /* INC_ECGCODES_H_ */
