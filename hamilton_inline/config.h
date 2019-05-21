ab384921050a0a8224da04115b697e74839c098d
xiaywang <xiaywang@iis.ee.ethz.ch>
2019-05-21 04:13:19 -0700

    fixed #ifdefs #endifs

--- hamilton_inline/config.h
+++ hamilton_inline/config.h
@@ -3,7 +3,7 @@
 #include "performance.h"
 // how many sambles get passed to the beatDetectAndClassify and subsequent function
 // 7200 should be devisible by it, so it runs most smoothly
-//#define MAIN_BLOCK_SIZE 1000
+#define MAIN_BLOCK_SIZE 1000
 
 // define all version numbers
 #define QRSFILT_OPT 1
